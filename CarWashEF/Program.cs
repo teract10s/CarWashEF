c9aa33876a2b24490d6ed6b01a1fd4d7a2d0a6cc
Teract10s <trs.leonov@gmail.com>
2023-12-06 12:38:51 +0200

    added all

---
+++ CarWashEF/Program.cs
@@ -0,0 +1,29 @@
+﻿using CarWashEF.Data;
+using CarWashEF.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using CarWashEF.Repository;
+
+namespace CarWashEF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                User user = new User() { Id = 1, Nickname = "nick", Email = "trs@gmail.com", Password = "12345678" };
+                User userFromDb = UserRepository.CreateUser(user);
+                Console.WriteLine("Inserted user: " + userFromDb.ToString());
+                Console.WriteLine("By email: " + UserRepository.GetByEmail(user.Email).ToString());
+                UserRepository.DeleteById(userFromDb.Id);
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
